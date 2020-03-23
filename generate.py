3d4dc745a0cfccc0722e83bc58dd98c57aa17b49
saintno <tctien342@gmail.com>
2020-03-23 23:21:57 +0700

    fix typo

--- generate.py
+++ generate.py
@@ -5,16 +5,16 @@ parser = argparse.ArgumentParser(description='''
 NDK's Opencore themes builder''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-n', nargs='*', metavar='k=v',
                     help='Name of zip files')
-sh("rm -rf ./icons")
-sh("mkdir -p ./icons")
+sh("rm -rf ./Icons")
+sh("mkdir -p ./Icons")
 for file in os.listdir("./logo"):
     if file.endswith(".png"):
         sh("/usr/bin/sips -z {} {} ./logo/{}".format(256, 256, file))
         print(os.path.join("./logo/", file))
-        sh("cp ./logo/{} ./icons/{}.icns".format(file, file.split(".")[0]))
-sh("cp ./misc/* ./icons/")
+        sh("cp ./logo/{} ./Icons/{}.icns".format(file, file.split(".")[0]))
+sh("cp ./misc/* ./Icons/")
 if parser.parse_args().n and len(parser.parse_args().n) > 0:
     zipName = parser.parse_args().n[0]
 else:
     zipName = "build"
-sh("zip -r {}-$(date +%y%m%d)-themes.zip ./icons/*".format(zipName))
+sh("zip -r {}-$(date +%y%m%d)-themes.zip ./Icons/*".format(zipName))
