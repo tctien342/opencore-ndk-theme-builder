9ad60efd46008a0c40ffef1e6197ae362a46be78
saintno <tctien342@gmail.com>
2020-03-23 23:20:45 +0700

    build into icons files

--- generate.py
+++ generate.py
@@ -4,17 +4,17 @@ import argparse
 parser = argparse.ArgumentParser(description='''
 NDK's Opencore themes builder''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-n', nargs='*', metavar='k=v',
-                    help='Name of output zip files')
-sh("rm -rf ./output")
-sh("mkdir -p ./output")
-for file in os.listdir("./icons"):
+                    help='Name of zip files')
+sh("rm -rf ./icons")
+sh("mkdir -p ./icons")
+for file in os.listdir("./logo"):
     if file.endswith(".png"):
-        sh("/usr/bin/sips -z {} {} ./icons/{}".format(256, 256, file))
-        print(os.path.join("./icons/", file))
-        sh("cp ./icons/{} ./output/{}.icns".format(file, file.split(".")[0]))
-sh("cp ./misc/* ./output/")
+        sh("/usr/bin/sips -z {} {} ./logo/{}".format(256, 256, file))
+        print(os.path.join("./logo/", file))
+        sh("cp ./logo/{} ./icons/{}.icns".format(file, file.split(".")[0]))
+sh("cp ./misc/* ./icons/")
 if parser.parse_args().n and len(parser.parse_args().n) > 0:
     zipName = parser.parse_args().n[0]
 else:
     zipName = "build"
-sh("zip -j {}-$(date +%y%m%d)-themes.zip ./output/*".format(zipName))
+sh("zip -r {}-$(date +%y%m%d)-themes.zip ./icons/*".format(zipName))
